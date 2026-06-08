60f17ec3141ece506df0362e3e0b0125c17cc598
Steven Tompkins <steven@moderne.io>
2026-06-08 11:58:08 -0500

    test(kork-atlas): verify baseUnit MeterFilter behavior

--- kork/kork-atlas/src/test/java/io/moderne/spinnaker/kork/atlas/AtlasMetricsAutoConfigurationTest.java
+++ kork/kork-atlas/src/test/java/io/moderne/spinnaker/kork/atlas/AtlasMetricsAutoConfigurationTest.java
@@ -1,6 +1,8 @@
 package io.moderne.spinnaker.kork.atlas;
 
 import io.micrometer.atlas.AtlasMeterRegistry;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.config.MeterFilter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
@@ -41,5 +43,37 @@ class AtlasMetricsAutoConfigurationTest {
                     assertThat(context).doesNotHaveBean("moderneCommonTags");
                     assertThat(context).doesNotHaveBean("atlasBaseUnitTagCustomizer");
                 });
+    }
+
+    @Test
+    void baseUnitMeterFilter_addsSecondsToTimerWithoutExplicitUnit() {
+        Meter.Id id = new Meter.Id("http.server.requests", Tags.empty(), null, "desc", Meter.Type.TIMER);
+        Meter.Id mapped = AtlasMetricsAutoConfiguration.baseUnitMeterFilter().map(id);
+
+        assertThat(mapped.getTag("baseUnit")).isEqualTo("seconds");
+    }
+
+    @Test
+    void baseUnitMeterFilter_addsSecondsToLongTaskTimerWithoutExplicitUnit() {
+        Meter.Id id = new Meter.Id("foo.long", Tags.empty(), null, "desc", Meter.Type.LONG_TASK_TIMER);
+        Meter.Id mapped = AtlasMetricsAutoConfiguration.baseUnitMeterFilter().map(id);
+
+        assertThat(mapped.getTag("baseUnit")).isEqualTo("seconds");
+    }
+
+    @Test
+    void baseUnitMeterFilter_preservesExplicitBaseUnit() {
+        Meter.Id id = new Meter.Id("foo.bytes", Tags.empty(), "bytes", "desc", Meter.Type.GAUGE);
+        Meter.Id mapped = AtlasMetricsAutoConfiguration.baseUnitMeterFilter().map(id);
+
+        assertThat(mapped.getTag("baseUnit")).isEqualTo("bytes");
+    }
+
+    @Test
+    void baseUnitMeterFilter_doesNotAddTagToCounterWithoutBaseUnit() {
+        Meter.Id id = new Meter.Id("foo.events", Tags.empty(), null, "desc", Meter.Type.COUNTER);
+        Meter.Id mapped = AtlasMetricsAutoConfiguration.baseUnitMeterFilter().map(id);
+
+        assertThat(mapped.getTag("baseUnit")).isNull();
     }
 }
